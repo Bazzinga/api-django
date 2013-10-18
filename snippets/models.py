132ff7802e7de23beed2efeb56693f389a511c44
Rory Walker <redrory@gmail.com>
2013-10-18 14:36:08 -0500

    added requests

--- snippets/models.py
+++ snippets/models.py
@@ -10,7 +10,7 @@ class Snippet(models.Model):
   created = models.DateTimeField(auto_now_add = True)
   title = models.CharField(max_length = 100, blank = True, default = '')
   code = models.TextField()
-  linenos = models.TextField()
+  linenos = models.TextField(default = False)
   language = models.CharField(choices = LANGUAGE_CHOICES,
                               default = 'python',
                               max_length = 100)
