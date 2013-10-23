c223d11b2f4272bc64dc1cb1919fa0029f973376
Rory Walker <redrory@gmail.com>
2013-10-23 12:12:36 -0500

    added login

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,11 +1,13 @@
 from django.forms import widgets
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
   class Meta:
     model = Snippet
-    fields = ('id', 'title','code','linenos','language','style')
+    owner = serializers.Field(source='owner.username')
+    fields = ('id', 'title','code','linenos','language','style', 'owner')
 
   def restore_object(self, attrs, instance = None):
     """
@@ -27,3 +29,10 @@ class SnippetSerializer(serializers.ModelSerializer):
 
       #create New instance
       return Snippet(**attrs)
+
+class UserSerializer(serializers.ModelSerializer):
+  snippets = serializers.PrimaryKeyRelatedField(many=True)
+
+  class Meta:
+    model = User
+    fields = ('id', 'username', 'snippets')
